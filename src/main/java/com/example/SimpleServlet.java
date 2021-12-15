fc396fcfe8aac60ce09df79c892499a041db23ad
Mark Swatosh <mark.swatosh@gmail.com>
2021-12-14 22:14:45 -0600

    Adding security

--- src/main/java/com/example/SimpleServlet.java
+++ src/main/java/com/example/SimpleServlet.java
@@ -2,12 +2,15 @@ package com.example;
 
 import java.io.IOException;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.transaction.UserTransaction;
 
 @WebServlet("/SimpleServlet")
 public class SimpleServlet extends HttpServlet {
@@ -17,6 +20,16 @@ public class SimpleServlet extends HttpServlet {
 		ServletOutputStream out = response.getOutputStream();
 
 		out.println("<h1>SimpleServlet</h1>");
+		out.println("getAuthType: " + request.getAuthType());
+        out.println("getRemoteUser: " + request.getRemoteUser());
+        out.println("getUserPrincipal: " + request.getUserPrincipal());
+
+		try {
+			UserTransaction tx = InitialContext.doLookup("java:comp/UserTransaction");
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,9 +1,17 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="javax.transaction.UserTransaction,javax.naming.InitialContext" %>
 <html>
     <head>
         <title>Sample JSP</title>
     </head>
     <body>
         <p>Servlet Invoke: <a href="SimpleServlet">here</a></p>
+        <% 
+        String[] arr = {"What's up?", "Hello", "It's a nice day today!"}; 
+         String greetings = arr[(int)(Math.random() * arr.length)];	
+
+         UserTransaction tx = InitialContext.doLookup("java:comp/UserTransaction");
+        %>
+        <p><%= greetings %></p>
     </body>
 </html>
