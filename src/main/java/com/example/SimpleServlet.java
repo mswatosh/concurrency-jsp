5aceb5b72ae7d1193576dc0664905179f7831eb8
Mark Swatosh <mark.swatosh@gmail.com>
2021-12-15 11:50:23 -0600

    Servlet with Security

--- src/main/java/com/example/SimpleServlet.java
+++ src/main/java/com/example/SimpleServlet.java
@@ -1,16 +1,18 @@
 package com.example;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.transaction.UserTransaction;
+
 
 @WebServlet("/SimpleServlet")
 public class SimpleServlet extends HttpServlet {
@@ -18,18 +20,17 @@ public class SimpleServlet extends HttpServlet {
 	@Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ServletOutputStream out = response.getOutputStream();
+        out.println("(" + Thread.currentThread().getId() + ") getUserPrincipal: " + request.getUserPrincipal());
 
-		out.println("<h1>SimpleServlet</h1>");
-		out.println("getAuthType: " + request.getAuthType());
-        out.println("getRemoteUser: " + request.getRemoteUser());
-        out.println("getUserPrincipal: " + request.getUserPrincipal());
+		ExecutorService executor = Executors.newFixedThreadPool(3);
 
+		Future<String> future = executor.submit(() -> {
+				return new String("(" + Thread.currentThread().getId() + ") getUserPrincipal: " + request.getUserPrincipal());
+		});
 		try {
-			UserTransaction tx = InitialContext.doLookup("java:comp/UserTransaction");
-		} catch (NamingException e) {
-			// TODO Auto-generated catch block
+			out.println(future.get());
+		} catch (InterruptedException | ExecutionException e) {
 			e.printStackTrace();
 		}
-
     }
 }
